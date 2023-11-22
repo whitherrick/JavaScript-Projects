35ee9c6fed996080f700f484cbd8594bea489399
whitherrick <whitney.herrick18@gmail.com>
2023-11-22 15:01:14 -0800

    Created index.html and main.js
    First JavaScript project

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+//Window alert box//
+window.alert("Hello, World"); 
+
+//creating first string//
+var Sent1 = "This is the beginning of the string";
+
+//creating second string//
+var Sent2 = " and this is the end of the string"; 
+
+//Concatenating the variables//
+document.write(Sent1 + Sent2);
+
+//writing and expression//
+document.write(2 + 2);

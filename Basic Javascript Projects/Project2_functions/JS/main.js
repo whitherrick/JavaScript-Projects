0a8e6f45b69ed44803125ea9c303599dfaed7ea0
whitherrick <whitney.herrick18@gmail.com>
2023-11-22 16:07:38 -0800

    Added Project 2
    Added project two for functions. Along with main.js

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "Yay! It worked!";
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+function myFunction() {
+    var sentence = "This is a";
+    sentence += " concatenated string";
+    document.getElementById("Concatenate").innerHTML =sentence;
+}
